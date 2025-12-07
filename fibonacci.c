3fd6c58fd0d004b7a602ed5a23ca6363cf76e928
Ayoub <123956897+Jounaidayoub@users.noreply.github.com>
2025-12-07 10:45:36 +0100

    add iterfative version

--- fibonacci.c
+++ fibonacci.c
@@ -14,3 +14,30 @@ int fibonacci_rec(int n)
 
     return fibonacci_rec(n - 1) + fibonacci_rec(n - 2);
 }
+
+int fibonacci_iter(int n)
+{
+
+    if (n <= 1)
+    {
+        return n;
+    }
+
+    int count = 2;
+
+    int prev = 1;
+    int pprev = 0;
+
+    int fibo = 0;
+    while (count <= n)
+    {
+
+        fibo = pprev + prev;
+
+        pprev = prev;
+        prev = fibo;
+
+        count++;
+    }
+    return fibo;
+}
