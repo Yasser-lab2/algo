6e3fa9e8d6034b2e8247623afa286690130fa9fb
Ayoub <123956897+Jounaidayoub@users.noreply.github.com>
2025-12-07 12:17:54 +0100

    add seconds

--- main.c
+++ main.c
@@ -11,16 +11,17 @@ int main()
     // printf("Jour dans semaine : %d\n", jour_dans_semaine(day, month, year));
 
     // fibonacci
-    int n = 45;
+    int n = 40;
     clock_t start=clock();
     printf("fibonacci_iter de %d is %d \n", n, fibonacci_iter(n));
     clock_t end=clock();
     printf("clock for the iterfative version is : (%ld - %ld)=%ld \n",start,end,end-start);
-    
+    printf("estimatd time %f  \n", (float)(end-start) / CLOCKS_PER_SEC);
     
     clock_t startr=clock();
     printf("fibonacci_rec de %d is %d \n", n, fibonacci_rec(n));
     clock_t endr=clock();
     printf("clock for the rec version is : (%ld - %ld)=%ld \n",startr,endr,endr-startr);
+    printf("estimatd time %f  \n", (float)(endr-startr)/CLOCKS_PER_SEC);
     return 0;
 }
